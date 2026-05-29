93d4119a1a83802e48a70548eee1be8ef1eea337
spudnik-1 <alsigal01@gmail.com>
2026-05-28 23:33:33 -0400

    Adding files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # python-playground
+
+1. Install git on your machine.
+1. Create a GitHub account.
+1. Get **spudnik-1** to add your GitHub user to the repository as a collaborator. This will allow you to make changes here.
+1. `cd` to your `code` directory.
+1. Run `git clone https://github.com/spudnik-1/python-playground.git`. This copies the repository that is in GitHub to your machine.
+1. Open the `python-playground` directory in VS Code.
+1. Implement the functions in `complete_me.py`.
+1. Run the tests in `complete_me_test.py` to make sure your code works.
+1. Run 
+
---
+++ complete_me.py
@@ -0,0 +1,59 @@
+import json
+
+def greet(name):
+    '''
+    Given a string, return the string "Hello, <the string goes here>! You are amazing :)"
+    So given "Pickle" as input, the function should return "Hello, Pickle! You are amazing :)"
+    '''
+    
+    # PLEASE IMPLEMENT ME
+
+    return ""
+
+
+def how_hot_is_it(temp):
+    '''
+    Given an integer as input, return:
+
+    "ICE ICE BABY" if the input is 0 or below.
+    "Bring me a blanket" if the input is more than 0 but less than 20.
+    "Nice and cozy" if the input is more than 20 but less than 24.
+    "FEELIN' HOT HOT HOT" if the input is 24 or higher.
+    '''
+    
+    # PLEASE IMPLEMENT ME
+
+    return ""
+
+
+def print_all_numbers_between_one_and_twenty_divisible_by_three():
+    '''
+    Print to standard output all integers between 1 and 20 that are divisible by 3.
+    '''
+
+    # PLEASE IMPLEMENT ME
+    
+    print("-1")
+
+
+def do_you_need_a_cookie():
+    '''
+    Prompt the user for input with the question "Do you need a cookie?".
+    If the answer is anything other than "Yes", prompt the user again with the same question.
+    If the answer is "Yes", print "Go have a cookie. You deserve it :)" and stop taking user input.
+    ''' 
+    
+    # PLEASE IMPLEMENT ME
+
+    print("No :(")
+
+
+def find_num_bikes():
+    '''
+    Given the information in the file 'free_bike_status.json',
+    return the number of bikes that are not disabled and whose have 50% or more fuel.
+    ''' 
+    
+    # PLEASE IMPLEMENT ME
+
+    return -1
